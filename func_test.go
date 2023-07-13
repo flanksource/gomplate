0c779b42ae2746d48b3ef2046e500db12591c20d
Moshe Immerman <moshe@flanksource.com>
2023-07-13 15:59:03 +0300

    chore: drop unsafe functions

--- func_test.go
+++ func_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestSprigFuncs(t *testing.T) {
-	funcs := CreateFuncs(context.Background(), nil)
+	funcs := CreateFuncs(context.Background())
 	if _, ok := funcs["semver"]; !ok {
 		t.Errorf("semver function not found")
 	}
